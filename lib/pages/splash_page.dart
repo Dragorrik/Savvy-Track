e1612622891cf9fd3a695295843be0f079710d14
Md Aarik Enam <aarikanan@gmail.com>
2024-12-30 00:38:04 +0600

    ...

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,7 +1,6 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:savvy_track/pages/expense_page.dart';
 import 'package:savvy_track/pages/login_page.dart';
 
 class SplashScreen extends StatefulWidget {
